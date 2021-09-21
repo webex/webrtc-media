3c07696abd89d6c09a28ade9f22bde7ef0730e3d
Jonnie Spratley <jspratle@cisco.com>
2021-09-21 14:34:38 -0700

    build(docs): add api-extractor

--- pipeline/release.sh
+++ pipeline/release.sh
@@ -5,6 +5,9 @@ nvm use
 echo "--------------------------------------------------"
 echo "Releasing code..."
 yarn release
-
 echo "--------------------------------------------------"
 echo "Code has been released"
+
+echo "--------------------------------------------------"
+echo "Building docs..."
+yarn docs && yarn docs:publish
