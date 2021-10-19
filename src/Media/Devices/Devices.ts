aa97aa8d7bdcefebaed3cba259062b078df12527
Taymoor Khan <tmkhan.k2@gmail.com>
2021-10-19 11:40:11 -0700

    refactor(devices): move get functions from devices to media/index

--- src/Media/Devices/Devices.ts
+++ src/Media/Devices/Devices.ts
@@ -1,61 +1,9 @@
 // eslint-disable-next-line no-shadow
-enum DeviceKinds {
+export enum DeviceKinds {
   VideoInput = 'videoinput',
   AudioInput = 'audioinput',
   AudioOutput = 'audiooutput'
 }
 
 /** @public */
-export default class Devices {
-  /**
-   * Requests a list of the available media input and output devices, such as microphones and cameras.
-   *
-   * @returns Promise Array of MediaDeviceInfo objects
-   * @public
-   */
-  static async getDevices(): Promise<MediaDeviceInfo[]> {
-    if (!navigator.mediaDevices?.enumerateDevices) {
-      console.warn('nativator.mediaDevices.enumerateDevices() is not supported.');
-
-      return [];
-    }
-
-    return navigator.mediaDevices.enumerateDevices();
-  }
-
-  /**
-   * Handles getting a list of video input devices
-   *
-   * @returns Promise Array of MediaDeviceInfo objects
-   * @public
-   */
-  static async getCameras(): Promise<MediaDeviceInfo[]> {
-    const devices = await Devices.getDevices();
-
-    return devices.filter(({kind}) => kind === DeviceKinds.VideoInput);
-  }
-
-  /**
-   * Handles getting a list of audio output devices
-   *
-   * @returns Promise Array of MediaDeviceInfo objects
-   * @public
-   */
-  static async getSpeakers(): Promise<MediaDeviceInfo[]> {
-    const devices = await Devices.getDevices();
-
-    return devices.filter(({kind}) => kind === DeviceKinds.AudioOutput);
-  }
-
-  /**
-   * Handles getting a list of audio input devices
-   *
-   * @returns Promise Array of MediaDeviceInfo objects
-   * @public
-   */
-  static async getMicrophones(): Promise<MediaDeviceInfo[]> {
-    const devices = await Devices.getDevices();
-
-    return devices.filter(({kind}) => kind === DeviceKinds.AudioInput);
-  }
-}
+export default class Devices {}
--- src/Media/index.ts
+++ src/Media/index.ts
@@ -1,2 +1,56 @@
+import {DeviceKinds} from './Devices/Devices';
+
+/**
+   * Requests a list of the available media input and output devices, such as microphones and cameras.
+   *
+   * @returns Promise Array of MediaDeviceInfo objects
+   * @public
+   */
+export const getDevices = async (): Promise<MediaDeviceInfo[]> => {
+  if (!navigator.mediaDevices?.enumerateDevices) {
+    console.warn('nativator.mediaDevices.enumerateDevices() is not supported.');
+
+    return [];
+  }
+
+  return navigator.mediaDevices.enumerateDevices();
+};
+
+/**
+ * Handles getting a list of video input devices
+ *
+ * @returns Promise Array of MediaDeviceInfo objects
+ * @public
+ */
+export const getCameras = async (): Promise<MediaDeviceInfo[]> => {
+  const devices = await getDevices();
+
+  return devices.filter(({kind}) => kind === DeviceKinds.VideoInput);
+};
+
+/**
+ * Handles getting a list of audio output devices
+ *
+ * @returns Promise Array of MediaDeviceInfo objects
+ * @public
+ */
+export const getSpeakers = async (): Promise<MediaDeviceInfo[]> => {
+  const devices = await getDevices();
+
+  return devices.filter(({kind}) => kind === DeviceKinds.AudioOutput);
+};
+
+/**
+ * Handles getting a list of audio input devices
+ *
+ * @returns Promise Array of MediaDeviceInfo objects
+ * @public
+ */
+export const getMicrophones = async (): Promise<MediaDeviceInfo[]> => {
+  const devices = await getDevices();
+
+  return devices.filter(({kind}) => kind === DeviceKinds.AudioInput);
+};
+
 export {default} from './Devices';
 export * from './Track';
