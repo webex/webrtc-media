25710bf62b6406aa96e22942818c6193f33874ef
Rajesh Kumar <rarajes2@cisco.com>
2022-08-08 15:55:06 +0530

    fix: build fix for cjs and esm

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,14 @@
-import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import nodePolyfills from 'rollup-plugin-polyfill-node';
+import commonjs from '@rollup/plugin-commonjs';
+import {babel} from '@rollup/plugin-babel';
 
 // Note: Order of plugins are important here - https://rollupjs.org/guide/en/#build-hooks
 const basePlugins = [
   commonjs(),
   nodePolyfills(),
   resolve({browser: true, extensions: ['.js', '.ts']}),
+  babel({babelHelpers: 'runtime', plugins: ['@babel/plugin-transform-runtime']}),
 ];
 
 export default {
@@ -15,6 +17,7 @@ export default {
     {format: 'esm', dir: 'dist/esm'},
     {format: 'umd', name: 'webrtcCore', file: './samples/bundle.js'},
   ],
+  external: [/@babel\/runtime/],
   plugins: basePlugins,
   watch: {include: 'src/**'},
 };
